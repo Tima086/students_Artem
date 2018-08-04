377f03a9dde29ca6acd6e99c089fe5f6f87d154e
Артём <Tima086@i.ua>
2018-08-04 18:11:48 +0300

    v 1.0.0

--- README.md
+++
@@ -1,5 +0,0 @@
-Sample for my students
-
-Try use it with Chrome DevTools
-
-https://garevna.github.io/devtools-sample/
--- index.js
+++
@@ -1,8 +0,0 @@
-var circle = document.querySelector ( ".circle" )
-
-var button = document.querySelector ( "button" )
-
-button.onclick = function ( event ) {
-        circle.style.visibility = circle.style.visibility === "visible" ?
-                                     "hidden" : "visible"
-}
